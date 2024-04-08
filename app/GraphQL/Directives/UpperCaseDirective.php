6f4d826a3686596575058a00376fd23ad3e98052
Alex <dizzersee@gmail.com>
2024-04-08 20:40:28 +0200

    Custom directive

---
+++ app/GraphQL/Directives/UpperCaseDirective.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Directives;
+
+use Nuwave\Lighthouse\Execution\ResolveInfo;
+use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
+use Nuwave\Lighthouse\Schema\Values\FieldValue;
+use Nuwave\Lighthouse\Support\Contracts\FieldMiddleware;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+final class UpperCaseDirective extends BaseDirective implements FieldMiddleware
+{
+    // TODO implement the directive https://lighthouse-php.com/master/custom-directives/getting-started.html
+
+    public static function definition(): string
+    {
+        return /** @lang GraphQL */ <<<'GRAPHQL'
+directive @upperCase on FIELD_DEFINITION
+GRAPHQL;
+    }
+
+
+    public function handleField(FieldValue $fieldValue): void
+    {
+        $fieldValue->wrapResolver(fn (callable $resolver): \Closure => function (mixed $root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) use ($fieldValue, $resolver) {
+
+            $result = $resolver($root, $args, $context, $resolveInfo);
+
+            if (is_string($result)) {
+                return strtoupper($result);
+            }
+
+            return $result;
+        });
+    }
+}
